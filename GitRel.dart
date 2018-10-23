6cb930d3641b968d352de93b505bcc11b39941a5
JG <infojgmx@googlemail.com>
2018-10-23 11:25:30 -0400

    can process starred repos from a user

--- GitRel.dart
+++ GitRel.dart
@@ -4,38 +4,135 @@ import 'package:http/http.dart' as http;
 import 'package:webfeed/webfeed.dart';
 import 'dart:io';
 import 'package:timeago/timeago.dart' as timeago;
+import 'package:args/args.dart';
 
-void main() async {
-  var client = new http.Client();
+// Filename for saving dates
+const String updateDatesFilename = 'updateDates.json';
+// Add this to every github repo to get the feed url
+const String releasesAtomString = '/releases.atom';
+// Filename of textfile containing github repos
+const String reposFileName = 'repos.txt';
 
-  if(!File('repos.txt').existsSync()) {
-    print("ERROR: You need a repos.txt file with the urls of github repos in it.\n");
-    exit(-1);
+// Storing dates when we run the app so we know about 
+// new versions next time
+var lastCheckedDates;
+bool storeLastDates = false;
+const String storeLastDatesString = 'storeLastDates';
+// Map containing the update dates
+Map<String, String> dateMap = Map<String, String>();
+
+void main(List<String> arguments) async {
+  final client = new http.Client();
+
+  // If a username was provided in the arguments, parse that
+  final parser = ArgParser();
+  // -d flag means a file called updateDates.json is created
+  // and will store dates so next time script runs, we know if
+  // there's a new version. This file is stored in the RUN DIR!!
+  parser.addFlag(storeLastDatesString,abbr: 'd');
+  ArgResults argResults = parser.parse(arguments);
+  List<String> argRest = argResults.rest;
+
+  if (argResults[storeLastDatesString]){
+    storeLastDates = true;
+
   }
 
-  // Text file containing all the repos
-  List<String> lines = new File('repos.txt').readAsLinesSync();
+  List<String> starFeedUrls = [];
+  String githubUser;
+  if (argRest.isNotEmpty) {
+    // Get the starred repos from a user from the github api
+    githubUser = argRest[0];
+    String starUrl = "https://api.github.com/users/"+githubUser+"/starred";
+    var starWeb = await client.get(starUrl);
+
+    List jsonStar = jsonDecode(starWeb.body);
+
+    // Convert starred urls to a list of atom feeds
+    
+    jsonStar.forEach((curMap) {
+      String curUrl = curMap['html_url'];
+      starFeedUrls.add(curUrl);
+    });
+  }
+
+  client.close();
+
+  List<String> lines = [];
+  if (File(reposFileName).existsSync()) {
+      // Text file containing all the repos
+      lines = new File(reposFileName).readAsLinesSync();
+
+  }
+
 
   // If we checked before, read the updateDates.json
-  var lastCheckedDates;
-
-  try {
-    String jsonFile = File('updateDates.json').readAsStringSync();
-    lastCheckedDates = json.decode(jsonFile);
-  } catch (e) {
-    print(e);
+  if(storeLastDates) {
+      try {
+        print("\nChecking to see if a previous update dates file exists here "+Directory.current.path+"/"+updateDatesFilename);
+        if(File(updateDatesFilename).existsSync()){
+          print("---> It does! Yay! <---");
+          String jsonFile = File(updateDatesFilename).readAsStringSync();
+          lastCheckedDates = json.decode(jsonFile);
+        } else{
+          print("it does not exist, it will be created this time.");
+        }
+        
+      } catch (e) {
+        // There is no update dates file yet
+    
+      }
   }
 
-  // Map containing the update dates
-  Map<String, String> dateMap = Map<String, String>();
+  if(starFeedUrls.isNotEmpty){
+    print("\nProcessing Starred Repos from user "+githubUser);
+    await processListOfFeeds(starFeedUrls);
+  }
+  
+  if(lines.isNotEmpty){
+    print("\nProcessing Repos from file "+reposFileName);
+    await processListOfFeeds(lines);
+  }
+  
+  if(lines.isEmpty && starFeedUrls.isEmpty){
+    print("\nNo repos file was provided, nor was a gitHub user name");
+    print("\nUSAGE:");
+    print("dart GitRel.dart githubusername\n");
+  }
 
+  // Write the new dateMap to disk if -d flag was used
+  if(storeLastDates) {
+    print("\nSaving update dates to file "+Directory.current.path+"/"+updateDatesFilename);
+    String encodedJsonMap = json.encode(dateMap);
+    File(updateDatesFilename).writeAsStringSync(encodedJsonMap);
+  }
+
+  print("\n");
+  // Normal exit
+  exit(0);
+}
+
+/// Takes a list of github repos URLs [lines] and looks at
+/// their releases atom feeds. Will print the last version
+/// and the last update date.
+void processListOfFeeds(List<String> lines) async {
+  
+
+  var client = new http.Client();
   // For every repo, get the feed, and get the last version + updated date
   for (var line in lines) {
-    String feedUrl = line + "/releases.atom";
+    String feedUrl = line + releasesAtomString;
 
     // Get the Feed
     var curFeed = await client.get(feedUrl);
     var feed = new AtomFeed.parse(curFeed.body);
+
+    // Check if there are releases
+    if (feed.items.isEmpty) {
+      // Skip this one
+      continue;
+    }
+
     AtomItem item = feed.items.first;
 
     // Format the date for easy reading
@@ -43,8 +140,7 @@ void main() async {
     String easyToReadTime = timeago.format(updatedTime);
 
     print("-------------------------------------------------------");
-    print(feed.title);
-    print("Latest Version: \t" + item.title.toString());
+    print(feed.title.replaceFirst("Release notes from ", '')+", " + item.title.toString());
 
     // If an old date exists, get that date and compare to the new date
     if (lastCheckedDates != null) {
@@ -54,8 +150,7 @@ void main() async {
         String easyToReadLastTime = timeago.format(lastCheckedDateTime);
 
         if (lastCheckedDateTime == updatedTime) {
-          print(
-              "Still the same version as last time you checked :(");
+          print("Still the same version as last time you checked :(");
         } else {
           // NEW VERSION, mention it
           print("----> NEW VERSION IS HERE <----");
@@ -64,16 +159,10 @@ void main() async {
     }
     print("Last update: \t" + easyToReadTime);
 
-    print("\n");
-
     // Add to Map
     dateMap[feed.title] = item.updated;
   }
+  client.close();
 
-  // Write the new dateMap to disk
-  String encodedJsonMap = json.encode(dateMap);
-  File('updateDates.json').writeAsStringSync(encodedJsonMap);
 
-  // Normal exit
-  exit(0);
 }
